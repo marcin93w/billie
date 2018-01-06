1180398f54abf2903a241a80749677ab8f0116c9
marcin93w <marcin93w@gmail.com>
2018-01-06 16:25:57 +0100

    Returning debt data after added

--- src/debts-controller.js
+++ src/debts-controller.js
@@ -9,8 +9,11 @@ router.route('/add').post((req, res) => {
 
     usersManager.getUserData(body.psid)
         .then(user => {
-            debtManager.addDebt(user.id, body.threadId, body.debtType, body.amount);
-            res.status(200).send();
+            const debtId = debtManager.addDebt(user.id, body.threadId, body.debtType, body.amount);
+            res.status(200).send({
+                debtId: debtId,
+                userName: user.name,
+            });
         })
         .catch(error => res.status(500).send(error));
 });
@@ -21,7 +24,9 @@ router.route('/accept/:id').post((req, res) => {
     usersManager.getUserData(body.psid)
         .then(user => {
             debtManager.acceptDebt(req.params.id, user.id);
-            res.status(200).send();
+            res.status(200).send({
+                userName: user.name
+            });
         })
         .catch(error => res.status(500).send(error));
 });
