584f4201a5421e8043a3028119c7d2a18b08ab49
marcin93w <marcin93w@gmail.com>
2018-02-04 16:21:04 +0100

    UI improvements on debt page

--- frontend/src/components/AddDebt.vue
+++ frontend/src/components/AddDebt.vue
@@ -1,51 +1,57 @@
 <template>
-  <div class="adding-panel">
-        <div>
+    <div class="adding-panel">
+        <div class="action-buttons">
             <button class="button"  
                 v-bind:class="[debtType === debtTypes.LENT ? '' : 'button-outline' ]" 
                 v-on:click="setDebtType(false, false)"  
                 value="0" name="debtActionButton">
                 <span>{{lentText}}</span>
             </button>
-            <img class="arrow" src="../assets/arrow1.png" 
-                v-bind:class="[debtType === debtTypes.LENT || debtType === debtTypes.BORROWED_PAYOFF ? 'arrow-active' : 'arrow-inactive' ] 
-                " 
-            />
-            <div class="between-arrows">
-            <div class="avatar">
-                <img :src="userAvatarURL" :alt="userName" /> 
-            </div>  
-            <div class="butt-middle">         
             <button class="button"  
-                v-if="isContactAccepted"
+                v-if="isContactAccepted && balance < 0"
                 v-bind:class="[debtType === debtTypes.LENT_PAYOFF || debtType === debtTypes.BORROWED_PAYOFF ? '' : 'button-outline' ]" 
                 v-on:click="setDebtType(true)" 
                 value="2" name="debtActionButton">
                 <span>{{payoffText}}</span>
             </button>
-            </div>            
-            <div class="avatar" > 
-                 <img :src="contactAvatarURL" :alt="contactName"  /> 
+            <div class="avatars-panel">
+                <img class="arrow arrow-top" src="../assets/arrow1.png" 
+                    v-bind:class="[debtType === debtTypes.LENT || debtType === debtTypes.BORROWED_PAYOFF ? 'arrow-active' : 'arrow-inactive' ]" 
+                />
+                <div class="between-arrows">
+                    <div class="avatar avatar-left">
+                        <img :src="userAvatarURL" :alt="userName" /> 
+                    </div>           
+                    <div class="avatar avatar-right">
+                        <img :src="contactAvatarURL" :alt="contactName"  /> 
+                    </div>
+                </div>
+                <img class="arrow arrow-bottom" src="../assets/arrow2.png" 
+                    v-bind:class="[debtType === debtTypes.BORROWED || debtType === debtTypes.LENT_PAYOFF ? 'arrow-active' : 'arrow-inactive' ]"
+                />
             </div>
-            </div>
-            <img class="arrow" src="../assets/arrow2.png" 
-                v-bind:class="[debtType === debtTypes.BORROWED || debtType === debtTypes.LENT_PAYOFF ? 'arrow-active' : 'arrow-inactive' ]
-               " />
-                        <button class="button "  
+            <button class="button "  
                 v-bind:class="[debtType === debtTypes.BORROWED ? '' : 'button-outline' ]" 
                 v-on:click="setDebtType(false, true)" 
                 value="1" name="debtActionButton">
                 <span>{{borrowedText}}</span>
             </button>
+            <button class="button"  
+                v-if="isContactAccepted && balance > 0"
+                v-bind:class="[debtType === debtTypes.LENT_PAYOFF || debtType === debtTypes.BORROWED_PAYOFF ? '' : 'button-outline' ]" 
+                v-on:click="setDebtType(true)" 
+                value="2" name="debtActionButton">
+                <span>{{payoffText}}</span>
+            </button>
         </div>
-        <div>
+        <div class="amount-panel">
             <input type="number" id="amount" v-model="amount" /> 
             <span class="currency-text">zł</span>
         </div>
         <div>
             <button @click="add()">Dodaj</button>
         </div>
-  </div>
+    </div>
 </template>
 
 <script>
@@ -75,8 +81,8 @@ function getDebtType (isPayoff, isBorrowed, currentAmount) {
 
 export default {
     name: 'AddDebt',
-    props: ['userName', 'userGender', 'userAvatar', 'showPayoff', 'isContactAccepted', 
-            'contactName', 'contactGender', 'contactAvatar', 'balance'],
+    props: ['userName', 'userGender', 'userAvatar', 'showPayoff', 'isContactAccepted',
+        'contactName', 'contactGender', 'contactAvatar', 'balance'],
     computed: {
         lentText: function () {
             if (this.isContactAccepted || this.contactName) {
@@ -100,10 +106,10 @@ export default {
             }
         },
         userAvatarURL: function () {
-         return this.userAvatar || avatar
+            return this.userAvatar || avatar
         },
         contactAvatarURL: function () {
-         return this.contactAvatar || avatar
+            return this.contactAvatar || avatar
         }
     },
     data () {
@@ -132,11 +138,19 @@ export default {
 
 <style scoped>
 .adding-panel {
-    margin-top: 40px;
+    margin-top: 20px;
 }
 
 .adding-panel div:not(.between-arrows, .avatar) {
     margin: 20px 10px;
+}
+
+.action-buttons {
+    margin: 20px auto;
+}
+
+.amount-panel {
+    margin: 20px auto;
 }
 
 #amount, .currency-text {
@@ -150,40 +164,50 @@ export default {
 .currency-text {
     margin-left: 5px;
 }
-.butt-middle{
-display: inline-block;
+
+.avatar {
+    margin: auto; 
+    width: 45px;
+    vertical-align: middle;
+    display: inline-block;
+    margin: auto 25px;
 }
-.butt-middle button{
-    margin: 0 15px 0 15px;
+
+.avatar-left {
+    margin-right: 75px;
 }
-@media screen  and (min-width: 500px) {
-    .butt-middle button{
-    margin: 0 50px 0 50px;
+
+.avatar-right {
+    margin-left: 75px;
 }
-}
-.avatar{
-margin: auto; 
-width: 45px;
-vertical-align: middle;
-display: inline-block;
-}
+
 .avatar img {
-border-radius: 50%;
+    border-radius: 50%;
 }
-.arrow{
-    width: 30%;
-    max-width: 300px;
+
+.arrow {
+    width: 125px;
     margin: auto auto 1.0rem auto;
     display: block;
 }
+
+.arrow-top {
+    margin-bottom: -5px;
+}
+
+.arrow-bottom {
+    margin-top: -5px;
+}
+
 .arrow-active{
     opacity: 1;
 }
+
 .arrow-inactive{
-     opacity: 0.2;   
-}
-.between-arrows{
-display: inline-block;
+    opacity: 0.2;   
 }
 
+.between-arrows{
+    display: inline-block;
+}
 </style>
