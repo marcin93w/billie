c1e594021349a4f105eaef8862ae4a5e82426be6
marcin93w <marcin93w@gmail.com>
2018-02-14 00:55:59 +0100

    Updated urls for PROD environment

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,6 @@
 export default {
-    homeUrl: 'https://messenger-debt-bot.herokuapp.com/',
-    fbAppId: '135139390498529',
+    // FIXME nasty hack to detect PROD environment
+    homeUrl: process.env.RDS_HOSTNAME ? 'https://billie.money/' : 'https://messenger-debt-bot.herokuapp.com/',
+    fbAppId: process.env.RDS_HOSTNAME ? '970214263126821' : '135139390498529',
     apiUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:1337' : ''
 }
