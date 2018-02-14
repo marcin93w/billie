4c6d4ea7a6d0f13dba9880eaa583ac50838051eb
marcin93w <marcin93w@gmail.com>
2018-02-14 23:17:42 +0100

    Fixed config for prod

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,5 @@
 export default {
-    // FIXME nasty hack to detect PROD environment
-    homeUrl: process.env.RDS_HOSTNAME ? 'https://billie.money/' : 'https://messenger-debt-bot.herokuapp.com/',
-    fbAppId: process.env.RDS_HOSTNAME ? '970214263126821' : '135139390498529',
+    homeUrl: process.env.NODE_ENV === 'production' ? 'https://billie.money/' : 'https://messenger-debt-bot.herokuapp.com/',
+    fbAppId: process.env.NODE_ENV === 'production' ? '970214263126821' : '135139390498529',
     apiUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:1337' : ''
 }
