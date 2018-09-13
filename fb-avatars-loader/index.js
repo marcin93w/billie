0a2a0d162bb10d2dde7302234c12389536d5eb54
marcin93w <marcin93w@gmail.com>
2018-09-13 21:03:25 +0200

    Update avatars script

--- fb-avatars-loader/index.js
+++ fb-avatars-loader/index.js
@@ -2,12 +2,11 @@ var request = require('request-promise');
 var PAGE_ACCESS_TOKEN = process.argv[2]
 
 function fetchUserData (senderPsid) {
-    //console.log(senderPsid)
     return request({
             url: "https://graph.facebook.com/v2.6/" + senderPsid,
             qs: {
                 access_token: PAGE_ACCESS_TOKEN,
-                fields: "first_name,last_name,gender,profile_pic"
+                fields: "profile_pic"
             },
             method: "GET"
         })
@@ -15,7 +14,7 @@ function fetchUserData (senderPsid) {
             const bodyObj = JSON.parse(body);
             console.log(`UPDATE public.users SET avatar_url = '${bodyObj.profile_pic}' WHERE psid = '${bodyObj.id }';`)
         })
-        .catch(err => {});
+        .catch(err => {  });
 }
 
 let psids = process.argv[3].split(',')
@@ -23,3 +22,4 @@ let psids = process.argv[3].split(',')
 psids.forEach(psid => {
     fetchUserData(psid)
 });
+
