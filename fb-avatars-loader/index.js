8190f8fddae49450a041e2bd9954c270ba1af5f8
marcin93w <marcin93w@gmail.com>
2018-10-29 21:50:57 +0100

    Implemented AWS Lambda function to update avatars

--- fb-avatars-loader/index.js
+++ fb-avatars-loader/index.js
@@ -1,25 +1,50 @@
-var request = require('request-promise');
-var PAGE_ACCESS_TOKEN = process.argv[2]
+const request = require('async-request');
+const { Client } = require('pg')
 
-function fetchUserData (senderPsid) {
-    return request({
-            url: "https://graph.facebook.com/v2.6/" + senderPsid,
-            qs: {
-                access_token: PAGE_ACCESS_TOKEN,
-                fields: "profile_pic"
-            },
-            method: "GET"
-        })
-        .then(function(body) {
-            const bodyObj = JSON.parse(body);
-            console.log(`UPDATE public.users SET avatar_url = '${bodyObj.profile_pic}' WHERE psid = '${bodyObj.id }';`)
-        })
-        .catch(err => {  });
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN
+const DB_PASSWORD = process.env.DB_PASSWORD
+
+function getDbConnection() {
+    const conn = `postgres://postgers:${DB_PASSWORD}@aa1798jl5zasldu.ckoqxmg442un.us-east-2.rds.amazonaws.com/postgres`;
+    const client = new Client(conn);
+    return client;
 }
 
-let psids = process.argv[3].split(',')
+async function fetchPsids(dbClient) {
+    var result = await dbClient.query("SELECT psid FROM public.users");
+    return result.rows.map(r => r.psid)
+}
 
-psids.forEach(psid => {
-    fetchUserData(psid)
-});
+async function fetchUserData (senderPsid) {
+    const response = await request({
+        url: "https://graph.facebook.com/v2.6/" + senderPsid,
+        qs: {
+            access_token: PAGE_ACCESS_TOKEN,
+            fields: "profile_pic"
+        },
+        method: "GET"
+    })
+    
+    const content = JSON.parse(response);
+    console.log(`UPDATE public.users SET avatar_url = '${content.profile_pic}' WHERE psid = '${content.id }';`)
+
+    return content
+}
+
+exports.handler = async function(event, context) {
+    const dbClient = getDbConnection()
+    await dbClient.connect()
+    const psids = await fetchPsids(dbClient)
+    console.log(JSON.stringify(psids))
+    await dbClient.end()
+
+    context.succeed(psids)
+}
+
+
+// let psids = process.argv[3].split(',')
+
+// psids.forEach(psid => {
+//     fetchUserData(psid)
+// });
 
