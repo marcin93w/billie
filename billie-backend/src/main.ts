41a72f6015fb89a3b37905118296282ac4935701
marcin93w <marcin93w@gmail.com>
2019-09-08 15:03:50 +0200

    Added hosting for frontend files

--- billie-backend/src/main.ts
+++ billie-backend/src/main.ts
@@ -1,17 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    allowedHeaders: [
-      'Content-Type',
-      'x-signed-request',
-      'x-user-id', // TODO remove in release version
-      'x-thread-id'], // TODO remove in release version
-    methods: ['GET', 'PUT', 'POST', 'DELETE'],
-    origin: 'http://localhost:8080',
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: {
+      allowedHeaders: ['Content-Type', 'x-signed-request'],
+      methods: ['GET', 'PUT', 'POST', 'DELETE'],
+      origin: 'http://localhost:8080',
+    },
   });
-  await app.listen(3000);
+
+  app.useStaticAssets(join(__dirname, '..', 'frontend/dist'), {
+    setHeaders: (res, path) => {
+      if (path.indexOf('index.html') !== -1) {
+        res.header('Cache-Control', 'public, no-cache');
+      }
+    },
+  });
+  app.useStaticAssets(join(__dirname, '..', 'assets'), { prefix: '/assets' });
+  app.getHttpAdapter().get('/privacy', (req, res) => {
+    res.redirect('https://termsfeed.com/privacy-policy/d69122341e558761c7a56e0ae60b28ef');
+  });
+
+  await app.listen(process.env.PORT || 3000);
 }
+
 bootstrap();
